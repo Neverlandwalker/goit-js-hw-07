6f512897f17277b0cde748d861325162415a8d05
Neverlandwalker <Blonnyya@gmail.com>
2022-08-31 20:34:21 +0300

    Initial commit

---
+++ js/02-lightbox.js
@@ -0,0 +1,32 @@
+import { galleryItems } from './gallery-items.js';
+
+const galleryRoot = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+galleryRoot.insertAdjacentHTML('beforeend', cardsMarkup);
+
+
+function createGalleryCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+
+        return `
+        <a class="gallery__item" 
+        href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" 
+  />
+</a>
+        `;
+    })
+        .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: 'bottom',
+
+
+});
+
+console.log(galleryItems);
